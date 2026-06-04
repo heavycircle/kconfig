bdabf26e95b42cd38b4ae8c6ebaaf09f53ce3d9c
heavycircle <87681482+heavycircle@users.noreply.github.com>
2026-06-04 13:39:59 -0400

    Update __init__.py

--- kconfig/core/utils/__init__.py
+++ kconfig/core/utils/__init__.py
@@ -1,24 +1,20 @@
 from __future__ import annotations
 
-from .fs import find_candidate_function_files, find_candidate_struct_files
+from .fs import find_candidate_function_files, find_candidate_kernel_modules, find_candidate_struct_files
 from .nodes import get_capture_nodes, get_capture_text, get_node_text
-from .normal import normalize_field, normalize_struct, normalize_type, sanitize_kernel_macros
 from .tables import print_struct_comparison
 from .treesitter import get_struct_members, parse_field_declaration, parse_field_declaration_list
 
 
 __all__ = [
     "find_candidate_function_files",
+    "find_candidate_kernel_modules",
     "find_candidate_struct_files",
     "get_capture_nodes",
     "get_capture_text",
     "get_node_text",
     "get_struct_members",
-    "normalize_field",
-    "normalize_struct",
-    "normalize_type",
     "parse_field_declaration",
     "parse_field_declaration_list",
     "print_struct_comparison",
-    "sanitize_kernel_macros",
 ]
