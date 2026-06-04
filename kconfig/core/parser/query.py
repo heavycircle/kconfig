e052737ad3d87a4d207bb0958e41e2d832ba9a48
heavycircle <87681482+heavycircle@users.noreply.github.com>
2026-06-04 13:06:40 -0400

    Update query.py

--- kconfig/core/parser/query.py
+++ kconfig/core/parser/query.py
@@ -19,7 +19,7 @@ def _normalize_query_name(name: str) -> str:
     if path.suffix == ".scm":
         return name
     if path.suffix:
-        raise ValueError(f"Invalid file extension: {name}")
+        raise KconfigFileInvalidError(name, "Invalid file extension")
     return f"{name}.scm"
 
 
@@ -34,7 +34,7 @@ def get_query(name: str) -> str:
         name (str): Name of the query file.
 
     Raises:
-        KconfigFileError: Cannot find query file.
+        KconfigFileNotFoundError: Cannot find query file.
 
     Returns:
         str: Text of the query.
@@ -42,15 +42,11 @@ def get_query(name: str) -> str:
     """
     project_root = Path(__file__).parent.parent.parent.parent
     if not project_root.exists():
-        raise KconfigFileError("Cannot find project root!")
+        raise KconfigFileNotFoundError(project_root)
 
-    check_dir = project_root / "kconfig"
-    if not (check_dir.exists() and check_dir.is_dir()):
-        raise KconfigFileError("Project root does not appear valid!")
-
-    query_path = check_dir / "queries" / _normalize_query_name(name)
+    query_path = project_root / "kconfig" / "queries" / _normalize_query_name(name)
     if not (query_path.exists() and query_path.is_file()):
-        raise KconfigFileError(f"Invalid query name: {name}")
+        raise KconfigFileNotFoundError(query_path)
 
     ui.out_debug(f"Found query file: {query_path.name}")
     return query_path.read_text()
@@ -64,7 +60,7 @@ def run_query(code: bytes, query: str) -> KconfigQueryResult:
         query (str): Tree-sitter (SCM) query.
 
     Raises:
-        KconfigQueryInvalidError: Invalid formatted SCM query.
+        KconfigQuerySyntaxError: Invalid formatted SCM query.
 
     Returns:
         KconfigQueryResult: Resulting structure of the query.
@@ -79,7 +75,7 @@ def run_query(code: bytes, query: str) -> KconfigQueryResult:
         cursor = tree_sitter.QueryCursor(query_obj)
         return cursor.matches(tree.root_node)
     except tree_sitter.QueryError as e:
-        raise KconfigQueryInvalidError(f"Invalid Query: {e}") from e
+        raise KconfigQuerySyntaxError(str(e)) from e
 
 
 def run_file_query(file: Path, query: str) -> KconfigQueryResult:
@@ -90,12 +86,12 @@ def run_file_query(file: Path, query: str) -> KconfigQueryResult:
         query (str): Tree-sitter (SCM) query.
 
     Raises:
-        KconfigFileError: Missing C or SCM file.
+        KconfigFileNotFoundError: Missing C file.
 
     Returns:
         KconfigQueryResult: Resulting structure of the query.
 
     """
     if not (file.exists() or file.is_file()):
-        raise KconfigFileError(f"No such file or directory: {file.name}")
+        raise KconfigFileNotFoundError(file)
     return run_query(file.read_bytes(), query)
