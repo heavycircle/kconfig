07c543a5b16dba4525061ffcda74a36d41f10cb2
heavycircle <87681482+heavycircle@users.noreply.github.com>
2026-06-04 13:08:08 -0400

    Update __init__.py

--- kconfig/core/structs/__init__.py
+++ kconfig/core/structs/__init__.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
+from .compare import analyze_struct_tree, compare_structure
 from .kernel import get_kernel_struct, get_kernel_struct_code
-from .module import get_module_struct
-from .utils import compare_structure
+from .module import get_module_capabilities, get_module_layout, get_module_struct
+from .utils import get_custom_struct_membes
 
 
 __all__ = [
+    "analyze_struct_tree",
     "compare_structure",
     "get_kernel_struct",
     "get_kernel_struct_code",
+    "get_module_capabilities",
+    "get_module_layout",
     "get_module_struct",
 ]
