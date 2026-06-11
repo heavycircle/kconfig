4e37867add4949d38ba6e898da55cb58ab6f8bb8
heavycircle <87681482+heavycircle@users.noreply.github.com>
2026-06-11 09:53:02 -0400

    Update kernel.py

--- kconfig/core/structs/kernel.py
+++ kconfig/core/structs/kernel.py
@@ -13,8 +13,10 @@ if TYPE_CHECKING:
 
     from tree_sitter import Node
 
+    from kconfig.types import KconfigStruct
 
-def find_struct_declaration(struct_name: str) -> tuple[Node, Path]:
+
+def find_struct_declaration(struct_name: str) -> tuple[Node, KconfigStruct]:
     """Find the declaration of a structure inside the kernel directory.
 
     Args:
@@ -25,12 +27,12 @@ def find_struct_declaration(struct_name: str) -> tuple[Node, Path]:
         KconfigSymbolNotFoundError: Struct not found in any candidate file.
 
     Returns:
-        tuple[Node, Path]: The struct's AST node and the file it was found in.
+        tuple[Node, KconfigStruct]: The struct's AST node and basic structure information.
 
     """
-    struct_file = cache.get_struct_location(struct_name)
-    if not struct_file:
-        raise KconfigSymbolNotFoundError(struct_name, state.kernel_dir)
+    struct_info = cache.get_struct_location(struct_name)
+    if not struct_info:
+        raise KconfigSymbolNotFoundError(struct_name, state.kernel_dir.name)
 
     contents = struct_file.read_bytes()
     for _, captures in parser.run_query("struct-list", contents):
@@ -39,23 +41,11 @@ def find_struct_declaration(struct_name: str) -> tuple[Node, Path]:
             continue
 
         found_name = struct_names[0].decode()
-        if found_name == struct_name:
+        if found_name == struct_info.resolved_name:
             rel_file = struct_file.relative_to(state.kernel_dir)
-            ui.out_debug(f"Found struct {struct_name} in {rel_file} ...")
+            struct_info.file = rel_file
+
+            ui.out_debug(f"Found struct '{struct_name}' in {rel_file} ...")
             return captures["struct.name"][0].parent, rel_file
-
-    ui.out_debug(f"Cannot find '{struct_name}', searching for aliases ...")
-    for file in utils.find_candidate_struct_files(state.kernel_dir, struct_name):
-        contents = file.read_bytes()
-        for _, captures in parser.run_query("alias-find", contents):
-            alias_names = utils.get_capture_text(captures, "alias.name")
-            if not alias_names:
-                continue
-
-            found_alias = alias_names[0].decode()
-            if found_alias == struct_name:
-                true_name = utils.get_capture_text(captures, "alias.target")[0].decode()
-                ui.out_debug(f"Resolved alias: {struct_name} -> {true_name}")
-                return find_struct_declaration(true_name)
 
     raise KconfigSymbolNotFoundError(struct_name, state.kernel_dir)
